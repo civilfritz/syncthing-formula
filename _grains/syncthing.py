17d5a4db97ffeab83382aa00dcb1ce6e864e742f
StartledPhoenix <phoenix@startledphoenix.com>
2014-12-29 21:20:35 -0500

    * Fixes #5

--- _grains/syncthing.py
+++ _grains/syncthing.py
@@ -2,6 +2,7 @@
 
 import sys
 import json
+import os.path
 import urllib2
 import salt.log
 import salt.utils
@@ -10,19 +11,20 @@ import salt.utils.network
 syncthing_api = "http://localhost:8080"
 
 def syncthing_data ():
+	#Check if syncthing service is online
+	if os.path.isfile("/etc/init/syncthing.conf"):
+		stdata = {}
 
-	stdata = {}
+		#Syncthing system data
+		syncthing_api_sys = urllib2.urlopen(syncthing_api + '/rest/system')
+		syncthing_api_sys_resp = syncthing_api_sys.read()
+		syncthing_api_sys_resp_json = json.loads(syncthing_api_sys_resp)
+		stdata['key'] =  syncthing_api_sys_resp_json["myID"]
 
-	#Syncthing system data
-	syncthing_api_sys = urllib2.urlopen(syncthing_api + '/rest/system')
-	syncthing_api_sys_resp = syncthing_api_sys.read()
-	syncthing_api_sys_resp_json = json.loads(syncthing_api_sys_resp)
-	stdata['key'] =  syncthing_api_sys_resp_json["myID"]
+		#Syncthing version data
+		syncthing_api_version = urllib2.urlopen(syncthing_api + '/rest/version')
+		syncthing_api_version_resp = syncthing_api_version.read()
+		syncthing_api_version_resp_json = json.loads(syncthing_api_version_resp)
+		stdata['ver'] = syncthing_api_version_resp_json["version"]
 
-	#Syncthing version data
-	syncthing_api_version = urllib2.urlopen(syncthing_api + '/rest/version')
-	syncthing_api_version_resp = syncthing_api_version.read()
-	syncthing_api_version_resp_json = json.loads(syncthing_api_version_resp)
-	stdata['ver'] = syncthing_api_version_resp_json["version"]
-
-	return {'syncthing': stdata}
+		return {'syncthing': stdata}
